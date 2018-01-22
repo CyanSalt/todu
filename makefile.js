d079145918103e7ccc4eb11a45ff9392a4cff6bb
孙翛然 <c.salt@hotmail.com>
2018-01-22 19:21:47 +0800

    Generate the icon file dynamically

--- makefile.js
+++ makefile.js
@@ -1,12 +1,28 @@
 const packager = require('electron-packager')
+const png2icons = require('png2icons')
+const path = require('path')
 const fs = require('fs')
+
+const ICON_PATH = 'src/resources/img/icon.ico'
+
+// Check icon file
+try {
+  fs.accessSync(ICON_PATH)
+} catch (error) {
+  console.log('Generating program icon...')
+  const folder = path.dirname(ICON_PATH)
+  const input = fs.readFileSync(`${folder}/icon.png`)
+  // eslint-disable-next-line new-cap
+  const output = png2icons.PNG2ICO_BMP(input, png2icons.BICUBIC, false)
+  fs.writeFileSync(ICON_PATH, output)
+}
 
 const options = {
   dir: '.',
   name: 'todu',
   out: 'dist/',
   overwrite: true,
-  icon: 'src/resources/img/icon.ico',
+  icon: ICON_PATH,
   ignore: [
     '^/(?!src|package\\.json|window\\.js)',
     '^/src/(storage|components|plugins)($|/)',
