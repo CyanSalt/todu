dff7c1a573575a0da1871a309ca8e805201ca044
孙翛然 <c.salt@hotmail.com>
2018-02-16 00:12:57 +0800

    Add internal locales support

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,8 +1,41 @@
+import {remote} from 'electron'
+import {readFileSync} from 'fs'
+import {resolve} from 'path'
 import {FileStorage} from './storage'
+
+const translations = [
+  {
+    file: 'zh-CN.json',
+    locales: ['zh', 'zh-CN'],
+  },
+  {
+    file: 'zh-TW.json',
+    locales: ['zh-TW'],
+  },
+]
+
+function load(file) {
+  const path = resolve(__dirname, 'assets/locales', file)
+  try {
+    return JSON.parse(readFileSync(path))
+  } catch (e) {
+    return null
+  }
+}
 
 export default {
   install(Vue, options) {
-    const dictionary = FileStorage.loadSync('translation.json') || {}
+    let locale = remote.app.getLocale()
+    const custom = FileStorage.loadSync('translation.json') || {}
+    if (custom['locale#!0']) locale = custom['locale#!0']
+    // Load translation data
+    const translation = translations
+      .find(({locales}) => locales.includes(locale))
+    const dictionary = (translation && load(translation.file)) || {}
+    // Merge user defined translation data
+    for (const [key, value] of Object.entries(custom)) {
+      if (value) dictionary[key] = value
+    }
     Vue.prototype.i18n = function (message) {
       return dictionary[message] || message.split('#!')[0]
     }
